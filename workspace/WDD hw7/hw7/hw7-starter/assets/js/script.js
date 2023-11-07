72b81c5bd30f536ac23f367360ead7b59c6029f7
danielamercad0 <144848650+danielamercad0@users.noreply.github.com>
2023-11-06 22:14:54 -0800

    funcs

--- workspace/WDD hw7/hw7/hw7-starter/assets/js/script.js
+++ workspace/WDD hw7/hw7/hw7-starter/assets/js/script.js
@@ -3,3 +3,12 @@ function averageThreeNumbers(a, b, c) {
     let average = sum / 3;
     return average;
 }
+
+function createSentence(num, noun) {
+    let sentence = "On average, a Berkeley student has" + " " + num + " " + noun + "s.";
+    return sentence;
+}
+
+function getRandomNum(max) {
+    return Math.random() * (max - 0) + 0;
+}
