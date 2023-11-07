aa8e04ccb2e2476ae41cc0a81f2c5ab99a0329a7
danielamercad0 <144848650+danielamercad0@users.noreply.github.com>
2023-11-06 22:34:13 -0800

    init

--- workspace/WDD hw7/hw7/hw7-starter/assets/js/script.js
+++ workspace/WDD hw7/hw7/hw7-starter/assets/js/script.js
@@ -12,3 +12,10 @@ function createSentence(num, noun) {
 function getRandomNum(max) {
     return Math.random() * (max - 0) + 0;
 }
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+let avg = averageThreeNumbers(x, y, z);
+let sentence = createSentence(avg, "sloth");
+console.log(sentence);
