027386feebf099515110d98785c2916ff35e0149
danielamercad0 <144848650+danielamercad0@users.noreply.github.com>
2023-11-13 17:16:56 -0800

    togg

--- workspace/WDD lab8/lab8/lab8-starter/script.js
+++ workspace/WDD lab8/lab8/lab8-starter/script.js
@@ -2,3 +2,6 @@ function toggleTheme() {
     let body = document.querySelector('body');
     body.classList.toggle('dark-mode');
 }
+
+let toggleButton = document.getElementById('toggleButton');
+toggleButton.onclick = toggleTheme;
